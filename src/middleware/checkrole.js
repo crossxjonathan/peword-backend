859803d725491d48f9cd69232e2e361a1eb93206
crossxjonathan <crossxjonathan@gmail.com>
2024-05-13 10:24:07 +0700

    readme

--- src/middleware/checkrole.js
+++ src/middleware/checkrole.js
@@ -1,26 +1,23 @@
 const { response } = require("../helper/common");
 const jwt = require('jsonwebtoken');
 
-const checkRole = (allowedRoles) => (req, res, next) => {
+const checkRole = (roles) => (req, res, next) => {
     const token = req.headers.authorization;
   
     if (!token) {
-      return response(res, null, 403, 'No token provided');
+      return response(res, null, 403, 'Token Unavailable');
     }
   
     jwt.verify(token.replace('Bearer ', ''), process.env.JWTSECRET, (err, decoded) => {
       if (err) {
-        console.error('JWT Verification Error:', err);
         return response(res, null, 403, 'Failed to authenticate token');
       }
-  
-      console.log('Decoded Token:', decoded);
-  
-      if (allowedRoles.includes(decoded.role)) {
+    
+      if (roles.includes(decoded.role)) {
         req.user = decoded;
         return response(res, decoded, 200, "Check Role Success")
       } else {
-        return response(res, null, 403, 'Insufficient permissions');
+        return response(res, null, 403, 'you have not a permission!!');
       }
     });
   };  
