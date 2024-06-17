e8765829cbbf8e6785df20f8c1f3232edb478f4a
crossxjonathan <crossxjonathan@gmail.com>
2024-06-17 09:55:09 +0700

    update vercel.json

--- src/middleware/checkrole.js
+++ src/middleware/checkrole.js
@@ -2,24 +2,24 @@ const { response } = require("../helper/common");
 const jwt = require('jsonwebtoken');
 
 const checkRole = (roles) => (req, res, next) => {
-    const token = req.headers.authorization;
-  
-    if (!token) {
-      return response(res, null, 403, 'Token Unavailable');
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return response(res, null, 403, 'Token Unavailable');
+  }
+
+  jwt.verify(token.replace('Bearer ', ''), process.env.JWTSECRET, (err, decoded) => {
+    if (err) {
+      return response(res, null, 403, 'Failed to authenticate token');
     }
-  
-    jwt.verify(token.replace('Bearer ', ''), process.env.JWTSECRET, (err, decoded) => {
-      if (err) {
-        return response(res, null, 403, 'Failed to authenticate token');
-      }
-    
-      if (roles.includes(decoded.role)) {
-        req.user = decoded;
-        return response(res, decoded, 200, "Check Role Success")
-      } else {
-        return response(res, null, 403, 'you have not a permission!!');
-      }
-    });
-  };  
+
+    if (roles.includes(decoded.role)) {
+      req.user = decoded;
+      return response(res, decoded, 200, "Check Role Success")
+    } else {
+      return response(res, null, 403, 'you have not a permission!!');
+    }
+  });
+};
 
 module.exports = checkRole
