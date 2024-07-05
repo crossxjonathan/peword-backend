cf191471ec22ddc828208fb3f68742fbeb219e68
crossxjonathan <crossxjonathan@gmail.com>
2024-07-05 15:20:58 +0700

    fix upload

--- src/controller/upload.controller.js
+++ src/controller/upload.controller.js
@@ -1,17 +1,21 @@
 const { response } = require("../helper/common");
 const cloudinary = require("../configs/cloudinary.config");
 
-const uploadSingle = async(req, res, next)=>{
-    try {
-      const result = await cloudinary.uploader.upload(req.file.path);
-      response(res, {file_url:result.secure_url }, 201, "Upload Image Success!!");
-    } catch (error) {
-      console.log(error);
-      next(error);
+const uploadSingle = async (req, res, next) => {
+  try {
+    if (!req.file || !req.file.path) {
+      throw new Error('No file uploaded');
     }
-    }
-    
+
+    const result = await cloudinary.uploader.upload(req.file.path);
+    response(res, { file_url: result.secure_url }, 201, "Upload Image Success!!");
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
+
 
 module.exports = {
-    uploadSingle,
+  uploadSingle,
 };
