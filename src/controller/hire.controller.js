fc3769c19b58ae585264b35746da785806a19700
crossxjonathan <crossxjonathan@gmail.com>
2024-07-09 17:51:18 +0700

    fix hire

--- src/controller/hire.controller.js
+++ src/controller/hire.controller.js
@@ -1,55 +1,78 @@
+/* eslint-disable no-unused-vars */
 const {response} = require('../helper/common');
-const { createHire, selectRecruiter, selectWorker } = require('../models/hire');
+const { createHire, selectWorkerById, selectRecruiterById } = require('../models/hire');
 
 
 // ADD HIRE
 const addHire = async (req, res, next) => {
-
-    const { message_purpose, name, email, phone, description, id, workers_id, recruiters_id } = req.body;
-
-    const data = {
-        id,
-        message_purpose, 
-        name, 
-        email,
-        phone,
-        description,
-        workers_id,
-        recruiters_id
-    };
+    const { message_purpose, name, email, phone, description, workers_id, recruiters_id } = req.body;
 
     try {
-        await createHire(data)
-        response(res, data, 201, 'Add Hire Successful!!')
+        const workerData = await selectWorkerById(workers_id);
+        const recruiterData = await selectRecruiterById(recruiters_id);
+
+        if (!workerData || !recruiterData) {
+            return response(res, null, 404, 'Worker or Recruiter not found');
+        }
+
+        const data = {
+            message_purpose,
+            name,
+            email,
+            phone,
+            description,
+            workers_id,
+            recruiters_id
+        };
+
+        const { rows } = await createHire(data);
+        const newHire = rows[0];
+        response(res, newHire, 201, 'Add Hire Successful!!');
     } catch (error) {
         console.log(error);
-        return response(res, data, 500, 'Something wrong in adding data, Try again!!')
-    };
+        return response(res, null, 500, 'Something wrong in adding data, Try again!!');
+    }
 };
-
 
 // GET RECRUITER
 const getRecruiter = async (req, res, next) => {
-
-    const { rows } = await selectRecruiter()
-    res.json({
-        status: '200',
-        data: rows,
-        message: 'get hire with recruiters success!!'
-    })
+    try {
+        const { rows } = await selectRecruiterById();
+        console.log(rows);
+        res.json({
+            status: '200',
+            data: rows,
+            message: 'get hire with recruiters success!!'
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            status: '500',
+            message: 'Error fetching hire data for recruiters'
+        });
+    }
 };
 
 
 // GET WORKER
 const getWorker = async (req, res, next) => {
-
-    const { rows } = await selectWorker()
-    res.json({
-        status: '200',
-        data: rows,
-        message: 'get hire with workers success!!'
-    })
+    try {
+        const { rows } = await selectWorkerById();
+        console.log(rows);
+        res.json({
+            status: '200',
+            data: rows,
+            message: 'get hire with workers success!!'
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            status: '500',
+            message: 'Error fetching hire data for workers'
+        });
+    }
 };
+
 
 
 module.exports = {
